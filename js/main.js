5c5e648d13d5d6c7bfd624ea1c1a2aac04c2795b
Shepard Summers <115957978+shepardsummers@users.noreply.github.com>
2023-12-10 18:29:21 -0500

    Working on Script

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var health = 0;
+var economy = 0;
+var growth = 0;
+var security = 0;
+var environment = 0;
+
+function opt1() {
+
+    document.querySelector("#check").innerHTML = "opt 1"
+
+}
+
+function opt2() {
+
+    document.querySelector("#check").innerHTML = "opt 2"
+
+}
