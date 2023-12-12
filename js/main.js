e471e2fdbefd49f3e7e8404353c52899811f6b97
Shepard Summers <115957978+shepardsummers@users.noreply.github.com>
2023-12-11 23:24:53 -0500

    added a bunch more stuff

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ var health = 20;
 var economy = 20;
 var growth = 20;
 var security = 20;
-var environment = 20;
+var environment = 80;
 
 cycle();
 
@@ -24,7 +24,57 @@ function cycle() {
             opt1text = "Agriculture";
             opt2text = "Pottery";
             break;
-    
+        
+        case 2:
+            opt1text = "Villages";
+            opt2text = "Weapons";
+            break;
+        
+        case 3:
+            opt1text = "Writing";
+            opt2text = "Boats";
+            break;
+
+        case 4:
+            opt1text = "Metallurgy";
+            opt2text = "Irrigation Systems";
+            break;
+        case 5:
+            opt1text = "Medicine";
+            opt2text = "Trade Networks";
+            break;
+        case 6:
+            opt1text = "Glassmaking";
+            opt2text = "Surgical Tools";
+            break;
+        case 7:
+            opt1text = "Sundials and Water Clocks";
+            opt2text = "Urban Planning";
+            break;
+        case 8:
+            opt1text = "";
+            opt2text = "";
+            break;
+        case 9:
+            opt1text = "";
+            opt2text = "";
+            break;
+        case 10:
+            opt1text = "";
+            opt2text = "";
+            break;
+        case 11:
+            opt1text = "";
+            opt2text = "";
+            break;
+        case 12:
+            opt1text = "";
+            opt2text = "";
+            break;
+        case 7000000:
+            opt1text = "";
+            opt2text = "";
+            break;
         default:
             opt1text = "error";
             opt2text = "error";
@@ -36,6 +86,18 @@ function cycle() {
     document.querySelector("#opt2").innerHTML = opt2text;
     
     if (health >= 100 || health <= 0) {
+        window.location.href = 'https://HON150PROJECT.github.io/lost';
+        document.querySelector("#error").innerHTML = "this code ran";
+    } else if (economy >= 100 || economy <= 0) {
+        window.location.href = 'https://HON150PROJECT.github.io/lost';
+        document.querySelector("#error").innerHTML = "this code ran";
+    } else if (growth >= 100 || growth <= 0) {
+        window.location.href = 'https://HON150PROJECT.github.io/lost';
+        document.querySelector("#error").innerHTML = "this code ran";
+    } else if (security >= 100 || security <= 0) {
+        window.location.href = 'https://HON150PROJECT.github.io/lost';
+        document.querySelector("#error").innerHTML = "this code ran";
+    } else if (environment >= 100 || environment <= 0) {
         window.location.href = 'https://HON150PROJECT.github.io/lost';
         document.querySelector("#error").innerHTML = "this code ran";
     } else {
@@ -70,9 +132,126 @@ function checkList(choice) {
 
         case 0:
             if (choice == true) {
-                health += 1;
+                growth += 5;
             } else if (choice == false) {
-                health = health - 1;
+                growth += 10;
+            } else {
+                document.querySelector("#error").innerhtml = "ERROR: 0, CHOICE ERROR"
+            }
+            break;
+        case 1:
+            if (choice == true) {
+                growth += 10;
+            } else if (choice == false) {
+                growth += 5;
+            } else {
+                document.querySelector("#error").innerhtml = "ERROR: 0, CHOICE ERROR"
+            }
+            break;
+        case 2:
+            if (choice == true) {
+                growth += 10;
+            } else if (choice == false) {
+                growth += (-5);
+                security += 10;
+            } else {
+                document.querySelector("#error").innerhtml = "ERROR: 0, CHOICE ERROR"
+            }
+            break;
+        case 3:
+            break;
+        case 4:
+            if (choice == true) {
+                economy += 10;
+                security += 10;
+            } else if (choice == false) {
+                growth += 10;
+                economy += 5;
+            } else {
+                document.querySelector("#error").innerhtml = "ERROR: 0, CHOICE ERROR"
+            }
+            break;
+        case 5:
+            if (choice == true) {
+                growth += 5;
+                health += 10;
+            } else if (choice == false) {
+                economy += 10;
+            } else {
+                document.querySelector("#error").innerhtml = "ERROR: 0, CHOICE ERROR"
+            }
+            break;
+        case 6:
+            if (choice == true) {
+                economy += 5;
+            } else if (choice == false) {
+                economy += 5;
+                health += 10;
+            } else {
+                document.querySelector("#error").innerhtml = "ERROR: 0, CHOICE ERROR"
+            }
+            break;
+        case 7:
+            if (choice == true) {
+                economy += 5;
+            } else if (choice == false) {
+                growth += 5;
+                security += 10;
+                economy += 5;
+            } else {
+                document.querySelector("#error").innerhtml = "ERROR: 0, CHOICE ERROR"
+            }
+            break;
+        case 8:
+            if (choice == true) {
+                
+            } else if (choice == false) {
+
+            } else {
+                document.querySelector("#error").innerhtml = "ERROR: 0, CHOICE ERROR"
+            }
+            break;
+        case 9:
+            if (choice == true) {
+                
+            } else if (choice == false) {
+
+            } else {
+                document.querySelector("#error").innerhtml = "ERROR: 0, CHOICE ERROR"
+            }
+            break;
+        case 10:
+            if (choice == true) {
+                
+            } else if (choice == false) {
+
+            } else {
+                document.querySelector("#error").innerhtml = "ERROR: 0, CHOICE ERROR"
+            }
+            break;
+        case 11:
+            if (choice == true) {
+                
+            } else if (choice == false) {
+
+            } else {
+                document.querySelector("#error").innerhtml = "ERROR: 0, CHOICE ERROR"
+            }
+            break;
+        case 12:
+            if (choice == true) {
+                
+            } else if (choice == false) {
+
+            } else {
+                document.querySelector("#error").innerhtml = "ERROR: 0, CHOICE ERROR"
+            }
+            break;
+        case 70000000:
+            if (choice == true) {
+                
+            } else if (choice == false) {
+
             } else {
                 document.querySelector("#error").innerhtml = "ERROR: 0, CHOICE ERROR"
             }
@@ -82,18 +261,53 @@ function checkList(choice) {
 
     }
 
-    document.querySelector("#health").innerHTML = "Health: " + health;
-    document.querySelector("#economy").innerHTML = "Economy: " + economy;
-    document.querySelector("#growth").innerHTML = "Growth: " + growth;
-    document.querySelector("#security").innerHTML = "Security: " + security;
-    document.querySelector("#environment").innerHTML = "Environment: " + environment;
-
-    //count += 1
+    
+    Era(count);
+    count += 1;
 
 }
 
 function guide() {
 
     window.location.href = 'https://HON150PROJECT.github.io/guide';
+
+}
+
+function Era(count) {
+
+    if (count <= 1) {
+        document.querySelector("#header").innerHTML = "TRIBAL ERA";
+        
+        document.querySelector("#growth").innerHTML = "Growth: " + growth;
+    } else if (count > 1 && count <= 3) {
+        document.querySelector("#header").innerHTML = "TRIBAL ERA";
+
+        document.querySelector("#growth").innerHTML = "Growth: " + growth;
+        document.querySelector("#security").innerHTML = "Security: " + security;
+    } else if (count > 3 && count <= 4) {
+        document.querySelector("#header").innerHTML = "BRONZE ERA";
+
+        document.querySelector("#growth").innerHTML = "Growth: " + growth;
+        document.querySelector("#security").innerHTML = "Security: " + security;
+        document.querySelector("#economy").innerHTML = "Economy: " + economy;
+    } else if (count > 4 && count <= 7) {
+        document.querySelector("#header").innerHTML = "BRONZE ERA";
+
+        document.querySelector("#growth").innerHTML = "Growth: " + growth;
+        document.querySelector("#security").innerHTML = "Security: " + security;
+        document.querySelector("#economy").innerHTML = "Economy: " + economy;
+        document.querySelector("#health").innerHTML = "Health: " + health;
+    } else if (count > 7 && count <= 12) {
+        document.querySelector("#header").innerHTML = "IRON ERA";
+        document.getElementById("mainImage").src = "res/medevil.jpg";
+
+        document.querySelector("#growth").innerHTML = "Growth: " + growth;
+        document.querySelector("#security").innerHTML = "Security: " + security;
+        document.querySelector("#economy").innerHTML = "Economy: " + economy;
+        document.querySelector("#health").innerHTML = "Health: " + health;
+    }
+
+
+    //document.querySelector("#environment").innerHTML = "Environment: " + environment;
 
 }
